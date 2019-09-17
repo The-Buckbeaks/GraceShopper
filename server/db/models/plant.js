bab568ed7d63e6627e885fe0723319d9ce60fe78
Siwin Lo <siwin.lo@gmail.com>
2019-09-17 11:27:02 -0400

    initial model definitions for User, Plant, Order

---
+++ server/db/models/plant.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Plant = db.define('plant', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.DECIMAL(10, 2)
+  },
+  quantity: Sequelize.INTEGER
+  //Siwin: We can add columns for "on sale", "light requirements", etc later
+})
+
+module.exports = Plant
+
+//Siwin: We need to add methods for checking out (i.e., decrement quantity in inventory etc)
--- server/db/models/user.js
+++ server/db/models/user.js
@@ -16,6 +16,8 @@ const User = db.define('user', {
       return () => this.getDataValue('password')
     }
   },
+  firstName: Sequelize.STRING,
+  lastName: Sequelize.STRING,
   salt: {
     type: Sequelize.STRING,
     // Making `.salt` act like a function hides it when serializing to JSON.
