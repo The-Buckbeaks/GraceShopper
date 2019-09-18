e186443208e698fbc64431d90a3bf7c612a6843f
petradish <49966727+petradish@users.noreply.github.com>
2019-09-18 11:08:57 -0400

    Update plants.js

--- server/api/plants.js
+++ server/api/plants.js
@@ -1,25 +1,25 @@
 const router = require('express').Router()
-const Plants = require('../db/models/plant')
+const Plant = require('../db/models/plant')
 
 router.get('/plants', async (req, res, next) => {
   try {
-    const allPlants = await Plants.findAll()
-    res.send(allPlants)
-  } catch (error) {
-    next(error)
+    const allPlants = await Plant.findAll()
+    res.json(allPlants)
+  } catch (err) {
+    next(err)
   }
 })
 
 router.get('/plants/:id', async (req, res, next) => {
   try {
-    const plant = await Plants.findOne({
+    const plant = await Plant.findOne({
       where: {
         id: req.params.id
       }
     })
-    res.send(plant)
-  } catch (error) {
-    next(error)
+    res.json(plant)
+  } catch (err) {
+    next(err)
   }
 })
 
