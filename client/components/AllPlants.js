774f86c71c8f70e0af14198df409a1f258d41ec2
petradish <49966727+petradish@users.noreply.github.com>
2019-09-18 10:56:01 -0400

    Update and rename Plants.js to AllPlants.js

--- client/components/AllPlants.js
+++ client/components/AllPlants.js
@@ -5,22 +5,22 @@ import {connect} from 'react-redux'
 import {getAllPlants} from '../store/index'
 //also need to import thunk from reducer
 
-class Plants extends React.Component {
+class AllPlants extends React.Component {
   componentDidMount() {
     this.props.getAllPlants()
   }
   render() {
     return (
       <div>
-        <h1 cla1="allPlants">All the Plants</h1>
-        <div className="Plants-Lists">
+        <h1 id="all-plants">All the Plants</h1>
+        <div className="plant-container">
           {this.props.plants.map(plant => (
             <div className="single-plant" key={plant.id}>
               <ul>
-                <h1> Plant Name:{plant.name}</h1>
-                <h3>Description:{plant.descripiton}</h3>
-                <h4> Price:{plant.price}</h4>
-                <img src={plant.imageUrl} alt="No Image" />
+                <h3>Plant Name: {plant.name}</h3>
+                <h4>Description: {plant.description}</h4>
+                <h5>Price:{plant.price}</h5>
+                <img src={plant.imgUrl} alt={plant.name} />
               </ul>
             </div>
           ))}
@@ -37,4 +37,4 @@ const mapDispatchToProps = dispatch => ({
   getAllplants: () => dispatch(getAllPlants())
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Plants)
+export default connect(mapStateToProps, mapDispatchToProps)(AllPlants)
