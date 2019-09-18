5af5830ceb648966b4080de708e34f61e49a9fae
Siwin Lo <siwin.lo@gmail.com>
2019-09-18 17:41:19 -0400

    feat: built SingleCartItem component

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import SingleCartItem from './SingleCartItem'
 
 class Cart extends Component {
   //create an order
@@ -10,27 +11,9 @@ class Cart extends Component {
         <div className="cart-title">
           <h1>Your Shopping Cart</h1>
         </div>
-        {this.props.cart.items.map(item => {
-          return (
-            <li className="cart-item" key={item.id}>
-              <div className="cart-image">
-                <img src={item.imgUrl} alt={item.name} />
-                <br />
-                <h2>
-                  <Link to={`/plants/${item.id}`}>{item.name}</Link>
-                </h2>
-                <p>
-                  <b>Quantity:</b>
-                  {item.quantity}
-                </p>
-                <p>
-                  <b>Price:</b>
-                  {item.price}
-                </p>
-              </div>
-            </li>
-          )
-        })}
+        {this.props.cart.items.map(item => (
+          <SingleCartItem key={item.id} item={item} />
+        ))}
       </div>
     ) : (
       <div className="cart-container">
