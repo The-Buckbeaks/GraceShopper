e6ed093e9332a44456826efeac4106862ccc23fa
bicipeng <bicipeng@gmail.com>
2019-09-22 22:25:38 -0400

    fixed: order specs

--- test/order.spec.js
+++
@@ -1,9 +0,0 @@
-const {expect} = require('chai')
-const db = require('../server/db/models')
-const Order = require('../server/db/models/plant')
-
-describe('Order model', () => {
-  beforeEach(() => {
-    return db.sync({force: true})
-  })
-})
