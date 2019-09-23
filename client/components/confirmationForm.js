0e0b700a1b42ae480fe6550020093a906402dbda
bicipeng <bicipeng@gmail.com>
2019-09-23 16:25:27 -0400

    fix:confirmationForm

--- client/components/confirmationForm.js
+++ client/components/confirmationForm.js
@@ -5,13 +5,18 @@ class confirmationForm extends React.Component {
     return (
       <div>
         <h1> Thank you for choosing The Octopus's Gardern!</h1>
-        <h3>WE'VE GOT YOUR ORDER,{this.props.user.id} .</h3>
+        {this.props.user.email ? (
+          <h3>WE'VE GOT YOUR ORDER,{this.props.user.id} .</h3>
+        ) : (
+          <h3>WE'VE GOT YOUR ORDER, DEAR!</h3>
+        )}
+
         <br />
         <h2>Shopping Summary</h2>
         <h3>Order Confirmation Number: {this.props.order.id}</h3>
+        <h3>Total Cost: {this.props.order.totalCost}</h3>
         <h3>Shipping Method: {this.props.order.shippingMethod}</h3>
-        <h3>Shipping to :</h3>
-        <h4>{this.props.order.address}</h4>
+        <h3>Shipping to: {this.props.order.address}</h3>
       </div>
     )
   }
