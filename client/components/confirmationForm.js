8fb1c8f0074066bd4d86f0c0b22264970de625cd
Siwin Lo <siwin.lo@gmail.com>
2019-09-24 18:03:38 -0400

    feat: styled auth form

--- client/components/confirmationForm.js
+++ client/components/confirmationForm.js
@@ -14,10 +14,10 @@ class confirmationForm extends React.Component {
         <br />
 
         <h2>Shopping Summary</h2>
-        <h3>Order Confirmation Number: {this.props.order.id}</h3>
-        <h3>Total Cost: ${(this.props.order.totalCost / 100).toFixed(2)}</h3>
-        <h3>Shipping Method: {this.props.order.shippingMethod}</h3>
-        <h3>Shipping to: {this.props.order.address}</h3>
+        <h3>🐙 Order Confirmation Number: {this.props.order.id}</h3>
+        <h3>💰 Total Cost: ${(this.props.order.totalCost / 100).toFixed(2)}</h3>
+        <h3>📬 Shipping Method: {this.props.order.shippingMethod}</h3>
+        <h3>🏡 Shipping to: {this.props.order.address}</h3>
       </div>
     )
   }
